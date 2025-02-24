93e08fecdb52e1e97ae062ff9c6d34a05ace5294
Rebar310 <rebar310@student.liu.se>
2025-02-24 17:16:51 +0100

    nu så är alla delar genomförda

---
+++ Lab2_Transformteori/Aterkopplat_System.m
@@ -0,0 +1,22 @@
+
+% Svar : En lätt partikel styrs lättast
+
+% En lätt partikel har mindre tröghet, vilket gör att den reagerar snabbare på styrsignalen 
+% u(t). Eftersom accelerationen ges av sambandet
+% a = F/m
+% så innebär en lägre massa att en given kraft genererar en större acceleration. 
+% Detta gör det enklare att korrigera partikelns rörelse och styra den mot målet.
+
+% motsatsen för tung partikel.
+
+
+% ----------------------------------------------
+
+% Svar: Återkoppling är generellt bättre än manuell styrning eftersom den ger en mer stabil och konsekvent reglering av partikeln mot slutvärdet 
+% x=1. Med en välvald förstärkningsparameter 
+% K anpassar sig systemet automatiskt och korrigerar avvikelser utan att man själv behöver justera styrsignalen hela tiden.
+% Vid för låg K når partikeln målet långsamt, medan för hög K kan orsaka översvängningar eller instabilitet. 
+% Manuell styrning kräver snabb reaktion och precision, vilket gör det svårare att hålla partikeln stabil, särskilt vid hög massa.
+
+transformservo
+
